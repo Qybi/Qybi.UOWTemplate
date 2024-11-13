d51ecf827f9e1c32e18f2586892dbbdb07ac7997
Raffaele <raffaele.trivillin@gmail.com>
2024-11-13 19:51:36 +0100

    feat(di): add context to dependency injection

--- Qybi.UOWTemplate/Program.cs
+++ Qybi.UOWTemplate/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Qybi.UOWTemplate.DataAccess.Contexts;
+using Qybi.UOWTemplate.DataAccess.Abstractions;
+using Qybi.UOWTemplate.DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("db"));
+});
 
 var app = builder.Build();
 
