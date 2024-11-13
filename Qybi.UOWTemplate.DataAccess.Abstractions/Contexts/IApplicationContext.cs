d95ec332395130a4523a987848144ab8d4ed01ff
Raffaele <raffaele.trivillin@gmail.com>
2024-11-13 19:50:20 +0100

    feat(context): add application context abstraction

---
+++ Qybi.UOWTemplate.DataAccess.Abstractions/Contexts/IApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Qybi.UOWTemplate.DataAccess.Abstractions.Contexts;
+
+public interface IApplicationContext
+{
+    EntityEntry Entry(object entity);
+    DbSet<TEntity> Set<TEntity>() where TEntity : class;
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
