a0fb38865e11fecdcaf5582d42ee99c459709e94
Raffaele <raffaele.trivillin@gmail.com>
2024-11-20 15:14:01 +0100

    refactor(repository): rename Find to Get, add CancellationToken to all async methods

--- Qybi.UOWTemplate.DataAccess.Abstractions/IRepository.cs
+++ Qybi.UOWTemplate.DataAccess.Abstractions/IRepository.cs
@@ -5,11 +5,11 @@ namespace Qybi.UOWTemplate.DataAccess.Abstractions;
 
 public interface IRepository
 {
-    Task<T?> GetById<T>(int id) where T : IEntity;
-    IQueryable<T> FindQueryable<T>(Expression<Func<T, bool>> expression, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null) where T : IEntity;
-    Task<List<T>> FindListAsync<T>(Expression<Func<T, bool>>? expression, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, CancellationToken cancellationToken = default) where T : class;
-    Task<List<T>> FindAllAsync<T>(CancellationToken cancellationToken) where T : IEntity;
-    Task<T?> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression, string includeProperties) where T : IEntity;
+    Task<T?> GetById<T>(int id, CancellationToken cancellationToken = default) where T : IEntity;
+    IQueryable<T> GetQueryable<T>(Expression<Func<T, bool>> expression, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null) where T : IEntity;
+    Task<List<T>> GetListAsync<T>(Expression<Func<T, bool>>? expression, Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null, CancellationToken cancellationToken = default) where T : class;
+    Task<List<T>> GetAllAsync<T>(CancellationToken cancellationToken = default) where T : IEntity;
+    Task<T?> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression, string includeProperties, CancellationToken cancellationToken = default) where T : IEntity;
     T Add<T>(T entity) where T : IEntity;
     void Update<T>(T entity) where T : IEntity;
     void UpdateRange<T>(IEnumerable<T> entities) where T : IEntity;
--- Qybi.UOWTemplate.DataAccess/Repository.cs
+++ Qybi.UOWTemplate.DataAccess/Repository.cs
@@ -16,19 +16,19 @@ public class Repository : IRepository
         _dbContext = dbContext;
     }
 
-    public async Task<T?> GetById<T>(int id) where T : IEntity
+    public async Task<T?> GetById<T>(int id, CancellationToken cancellationToken = default) where T : IEntity
     {
         return await _dbContext.Set<T>().FindAsync(id);
     }
 
-    public IQueryable<T> FindQueryable<T>(Expression<Func<T, bool>> expression,
+    public IQueryable<T> GetQueryable<T>(Expression<Func<T, bool>> expression,
         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null) where T : IEntity
     {
         var query = _dbContext.Set<T>().Where(expression);
         return orderBy != null ? orderBy(query) : query;
     }
 
-    public Task<List<T>> FindListAsync<T>(Expression<Func<T, bool>>? expression, Func<IQueryable<T>,
+    public Task<List<T>> GetListAsync<T>(Expression<Func<T, bool>>? expression, Func<IQueryable<T>,
         IOrderedQueryable<T>>? orderBy = null, CancellationToken cancellationToken = default)
         where T : class
     {
@@ -38,12 +38,12 @@ public class Repository : IRepository
             : query.ToListAsync(cancellationToken);
     }
 
-    public Task<List<T>> FindAllAsync<T>(CancellationToken cancellationToken) where T : IEntity
+    public Task<List<T>> GetAllAsync<T>(CancellationToken cancellationToken) where T : IEntity
     {
         return _dbContext.Set<T>().ToListAsync(cancellationToken);
     }
 
-    public Task<T?> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression, string includeProperties) where T : IEntity
+    public Task<T?> SingleOrDefaultAsync<T>(Expression<Func<T, bool>> expression, string includeProperties, CancellationToken cancellationToken = default) where T : IEntity
     {
         var query = _dbContext.Set<T>().AsQueryable();
 
