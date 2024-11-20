05f4e853e8029ebc044e000e4612ebf89a9555cd
Raffaele <raffaele.trivillin@gmail.com>
2024-11-20 14:52:25 +0100

    feat(mappers): add list mapping

--- Qybi.UOWTemplate.Helpers/ToDTOMapper.cs
+++ Qybi.UOWTemplate.Helpers/ToDTOMapper.cs
@@ -16,6 +16,17 @@ public class ToDTOMapper
             Category = product.Category
         };
     }
+    public static IEnumerable<ProductDTO> Map(IEnumerable<Product> products)
+    {
+        return products.Select(item => new ProductDTO
+        {
+            Code = item.Code,
+            Name = item.Name,
+            Description = item.Description,
+            Price = item.Price,
+            Category = item.Category
+        }).ToList();
+    }
 
     public static CategoryDTO Map(Category category)
     {
@@ -25,5 +36,15 @@ public class ToDTOMapper
             Description = category.Description,
             Products = category.Products
         };
+    }
+
+    public static IEnumerable<CategoryDTO> Map(IEnumerable<Category> categories)
+    {
+        return categories.Select(item => new CategoryDTO
+        {
+            Name = item.Name,
+            Description = item.Description,
+            Products = item.Products
+        }).ToList();
     }
 }
--- Qybi.UOWTemplate.Helpers/ToEntityMapper.cs
+++ Qybi.UOWTemplate.Helpers/ToEntityMapper.cs
@@ -14,8 +14,20 @@ public class ToEntityMapper
             Name = product.Name,
             Description = product.Description,
             Price = product.Price,
-            Category = product.Category
+            CategoryId = product.Category.Id
         };
+    }
+
+    public static IEnumerable<Product> Map(IEnumerable<ProductDTO> products)
+    {
+        return products.Select(product => new Product
+        {
+            Code = product.Code,
+            Name = product.Name,
+            Description = product.Description,
+            Price = product.Price,
+            CategoryId = product.Category.Id
+        });
     }
 
     public static Category Map(CategoryDTO category)
@@ -23,8 +35,16 @@ public class ToEntityMapper
         return new Category
         {
             Name = category.Name,
-            Description = category.Description,
-            Products = category.Products
+            Description = category.Description
         };
+    }
+
+    public static IEnumerable<Category> Map(IEnumerable<CategoryDTO> categories)
+    {
+        return categories.Select(category => new Category
+        {
+            Name = category.Name,
+            Description = category.Description
+        });
     }
 }
